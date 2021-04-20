a86685eebcdaeae1b3743d1d767801b80d511deb
Laio Masruha Pereira <laiomasruha@gmail.com>
2021-04-19 21:42:08 -0300

    Fixed _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,11 @@
 import "tailwindcss/tailwind.css";
 import Layout from './../components/Layout';
-import Login from './../pages/Login';
-
-const logado = false;
 
 function MyApp({ Component, pageProps }) {
     return (
-        logado
-            ? <Layout>
-                <Component{...pageProps} />
-            </Layout>
-            : <Login />
+        <Layout>
+            <Component{...pageProps} />
+        </Layout>
     )
 }
 
