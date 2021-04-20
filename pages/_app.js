5bfb3de8efe164f05f5be18c1aaea4c36a4ba6d9
Laio Masruha Pereira <laiomasruha@gmail.com>
2021-04-19 21:30:59 -0300

    Created _app.js file and modified index.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "tailwindcss/tailwind.css";
+import Layout from './../components/Layout';
+import Login from './../pages/Login';
+
+const logado = false;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        logado
+            ? <Layout>
+                <Component{...pageProps} />
+            </Layout>
+            : <Login />
+    )
 }
 
 export default MyApp
