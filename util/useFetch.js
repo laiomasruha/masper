e6a9c2cafc43910d947e3751adfea8546c56dbb3
Laio Masruha Pereira <66394759+laiomasruha@users.noreply.github.com>
2021-04-19 23:22:53 -0300

    Update useFetch.js

--- util/useFetch.js
+++ util/useFetch.js
@@ -4,7 +4,7 @@ const { NEXT_PUBLIC_VERCEL_URL } = process.env;
 
 export function useFetch(url) {
     const APIUrl = `${NEXT_PUBLIC_VERCEL_URL}/api/${url}`;
-    
+    console.log(APIUrl);
     const { data, error } = useSWR(APIUrl, async APIUrl => {
         const response = await fetch(APIUrl);
         const data = await response.json();
